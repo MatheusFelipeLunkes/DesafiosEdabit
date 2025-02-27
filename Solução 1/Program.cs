45c81bc7be5ea62ad0e80f61d0adc6f166075ae1
Matheus Lunkes <matheus.lunkes.corp@gmail.com>
2025-02-27 19:52:16 -0300

    Solução 1, ReverseCase - Trabalhando com strings

---
+++ Solução 1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+SOLUÇÃO 1: 
+Dada uma string, crie uma função para inverter a caixa. Todas as letras minúsculas devem ser maiúsculas, e vice-versa.
+
+Exemplos
+ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+
+ReverseCase("MANY THANKS") ➞ "many thanks"
+
+ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+ */
+
+string teste = "Happy Birthday";
+Console.WriteLine(ReverseCase(teste));
+teste = "many thanks";
+Console.WriteLine(ReverseCase(teste));
+teste = "SpOnTaNeOuS";
+Console.WriteLine(ReverseCase(teste));
+
+string ReverseCase(string str)
+{
+    string str2 = "";
+    foreach (var c in str)
+    {
+        if (char.IsUpper(c))
+        {
+            str2 += char.ToLower(c);
+        }
+        else
+        {
+            str2 += char.ToUpper(c);
+        }
+    }
+
+    return str2;
+}
+
+
+
+
+
